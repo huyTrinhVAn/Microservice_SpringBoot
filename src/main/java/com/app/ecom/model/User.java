ff24948eabbfbb3a2c40f44c0027f8ff270ccd2c
huyTrinhVAn <128719193+huyTrinhVAn@users.noreply.github.com>
2026-03-31 22:24:00 +1100

    fix(app): Organizing folder structure

--- src/main/java/com/app/ecom/model/User.java
+++ src/main/java/com/app/ecom/model/User.java
@@ -1,11 +1,10 @@
-package com.app.ecom;
+package com.app.ecom.model;
 
 import lombok.Data;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 @Data
