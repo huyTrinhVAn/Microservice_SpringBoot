cce739ec49a48e4eeb5b2536a6353786f0c8a2a2
huyTrinhVAn <128719193+huyTrinhVAn@users.noreply.github.com>
2026-03-31 21:58:45 +1100

    feat(app): JPA set up done

--- src/main/java/com/app/ecom/UserService.java
+++ src/main/java/com/app/ecom/UserService.java
@@ -1,37 +1,36 @@
 package com.app.ecom;
 
 import org.springframework.stereotype.Service;
-
+import lombok.RequiredArgsConstructor;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 @Service
+@RequiredArgsConstructor
 public class UserService {
-    private List<User> userList = new ArrayList<>();
-    private Long nextId = 1L;
+    private final UserRepository userRepository;
+//    private List<User> userList = new ArrayList<>();
+//    private Long nextId = 1L;
 
     public List<User> fetchAllUsers(){
-        return userList;
+        return userRepository.findAll();
     }
 
     public void addUser(User user){
-        user.setId(nextId++);
-        userList.add(user);
+//        user.setId(nextId++);
+        userRepository.save(user);
     }
 
 
     public Optional<User> fetchUser(Long id) {
-        return userList.stream()
-                .filter(user -> user.getId().equals(id))
-                .findFirst();
+        return userRepository.findById(id);
     }
     public boolean updateUser(Long id, User updatedUser) {
-        return userList.stream()
-                .filter(user -> user.getId().equals(id))
-                .findFirst()
+        return userRepository.findById(id)
                 .map(existingUser -> {
                     existingUser.setFirstName(updatedUser.getFirstName());
                     existingUser.setLastName(updatedUser.getLastName());
+                    userRepository.save(existingUser);
                     return true;
                 }).orElse(false);
     }
